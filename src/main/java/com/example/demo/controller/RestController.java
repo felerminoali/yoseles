39f0e6c9e637204168ab6335910ea73f60c1a9dc
felerminoali <felerminoali@gmail.com>
2020-03-09 09:06:14 +0200

    web service

---
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/ping")
+    public ResponseEntity<Object> ping(){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("alive", true);
+        return new ResponseEntity<Object>(map, HttpStatus.OK);
+    }
+}
